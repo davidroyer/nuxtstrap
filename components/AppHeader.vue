c4c88911cb287238df4ed59b07fecefd495bbdc2
David Royer <droyer01@gmail.com>
2018-04-21 10:46:52 -0400

    updated header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,8 +2,7 @@
   <header>
     <h1>NuxtStrap</h1>
     <AppNav/>
-    <!-- <h6 class="loggedIn-status">Logged In: <b-badge variant="info">{{$store.state.loggedIn}}</b-badge></h6> -->
-
+    <h6 class="loggedIn-status">Logged In: <b-badge variant="info">{{$store.state.loggedIn}}</b-badge></h6>
   </header>
 </template>
 
