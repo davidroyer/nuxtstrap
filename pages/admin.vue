5ae8bccee8ae786efbcf52679b192226b1f2a05b
David Royer <droyer01@gmail.com>
2018-04-21 11:34:51 -0400

    update again

--- pages/admin.vue
+++ pages/admin.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <div>
       <h1 class="title">Admin Page</h1>
-      <!-- <div>{{$store.state.user}}</div> -->
+      <div>{{$auth.user}}</div>
 
       <h2>Super Secret Info Here</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus tempora dicta fugiat dolore atque delectus alias, magni quos ullam rem, aliquid assumenda non aut at. Eaque recusandae, cupiditate vitae iusto repellendus omnis officia dignissimos natus modi, culpa unde temporibus illum.</p>
