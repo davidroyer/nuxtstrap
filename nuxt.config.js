916bcadaed19a5ed35787c091cb11b45e1eda228
David Royer <droyer01@gmail.com>
2018-04-23 07:53:09 -0400

    removed polyfill script from head

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,19 +7,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    // title: pkg.name,
     titleTemplate: '%s - NuxtStrap',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js' }
-    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      // { type: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js' }
     ]
   },
 
