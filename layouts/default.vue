25562f4a4d932e33b5647c78db34c9b8fd497e29
David Royer <droyer01@gmail.com>
2019-11-30 07:21:58 -0500

    chore: update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app-wrapper">
-    <AppHeader/>
-    <nuxt id="view" class="container"/>
-    <AppFooter/>
+    <!-- <AppHeader/> -->
+    <b-navbar toggleable="lg" type="dark" variant="dark" class="mb-3">
+      <b-container class="b-container-comp">
+        <n-link to="/" tag="b-navbar-brand">NuxtStrap</n-link>
+
+        <b-navbar-toggle target="nav-collapse" />
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item exact to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item to="/posts">Posts</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+    <nuxt id="view" class="container" />
+    <AppFooter />
   </div>
 </template>
 
@@ -19,3 +34,9 @@ export default {
 </script>
 
 <style src="@/assets/styles/transitions.scss" lang="scss"></style>
+
+<style>
+.navbar {
+  width: 100%;
+}
+</style>
