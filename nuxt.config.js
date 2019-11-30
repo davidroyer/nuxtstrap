52ddf75e6e044e842dc1e3a4acbbd581fb82431d
David Royer <droyer01@gmail.com>
2019-11-30 07:33:15 -0500

    fix(nuxt.config): adds generate fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
-const pkg = require('./package')
+const pkg = require("./package");
 // const bodyParser = require('body-parser')
-const session = require('express-session')
-require('dotenv').config()
+const session = require("express-session");
+require("dotenv").config();
 
 module.exports = {
   // mode: 'universal',
@@ -9,89 +9,89 @@ module.exports = {
     adminPassword: process.env.PASSWORD
   },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    titleTemplate: '%s - NuxtStrap',
+    titleTemplate: "%s - NuxtStrap",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   dir: {
-    middleware: 'router-middleware', // Default: 'middleware'
+    middleware: "router-middleware" // Default: 'middleware'
   },
 
   router: {
-    middleware: 'rendered'
+    middleware: "rendered"
   },
 
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: {
-    color: '#007bff',
+    color: "#007bff"
     // height: '5px'
   },
   // loading: { color: '#FFFFFF' },
 
   /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/styles/app.scss'
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/styles/app.scss"],
+
+  generate: {
+    fallback: true
+  },
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [,
-    '@nuxtjs/dotenv',
-    '@nuxtjs/toast',
-    ['bootstrap-vue/nuxt', { css: false }],
-    '@nuxtjs/device',
-    '@nuxtjs/axios'
+   ** Nuxt.js modules
+   */
+  modules: [
+    ,
+    "@nuxtjs/dotenv",
+    "@nuxtjs/toast",
+    ["bootstrap-vue/nuxt", { css: false }],
+    "@nuxtjs/device",
+    "@nuxtjs/axios"
   ],
 
   axios: {
-    baseURL: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : 'http://localhost:3000'
+    baseURL:
+      process.env.NODE_ENV === "production"
+        ? process.env.PRODUCTION_URL
+        : "http://localhost:3000"
   },
 
   toast: {
-    position: 'top-center',
+    position: "top-center",
     duration: 1500
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   },
   serverMiddleware: [
     session({
-      secret: 'super-secret-key',
+      secret: "super-secret-key",
       resave: false,
       saveUninitialized: false,
       cookie: { maxAge: 60000 }
     }),
     // Api middleware
     // We add /api/login & /api/logout routes
-    '~/api/index.js'
+    "~/api/index.js"
   ]
-}
+};
