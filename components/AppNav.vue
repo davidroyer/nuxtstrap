845b66311c56ae290ab27420c3ee49da9c31bf01
David Royer <droyer01@gmail.com>
2019-11-30 07:32:20 -0500

    chore: cleanup

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -5,17 +5,22 @@
       <b-nav-item to="/about">About</b-nav-item>
       <b-nav-item to="/posts">Posts</b-nav-item>
       <a href="/api/">API</a>
-      <b-button v-if="!$store.state.user " to="/login"  id="toggle-login" variant="primary" size="sm">Login</b-button>
+      <b-button
+        v-if="!$store.state.user "
+        to="/login"
+        id="toggle-login"
+        variant="primary"
+        size="sm"
+      >Login</b-button>
       <template v-else>
         <b-nav-item to="/admin">Admin</b-nav-item>
-        <b-button id="toggle-login" @click="$store.dispatch('adminLogout')" variant="secondary" size="sm">Logout</b-button>
+        <b-button
+          id="toggle-login"
+          @click="$store.dispatch('adminLogout')"
+          variant="secondary"
+          size="sm"
+        >Logout</b-button>
       </template>
     </b-nav>
   </nav>
 </template>
-
-<script>
-  export default {
-    name: 'AppNav'
-  }
-</script>
